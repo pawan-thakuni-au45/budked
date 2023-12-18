e2e4f5b7742e11fd1b1c999217bd4e55bf20b600
pawan <pawandaathakuni@gmail.com>
2023-12-18 18:02:37 +0530

    Rating commit

---
+++ frontend/src/component/Rating.js
@@ -0,0 +1,18 @@
+
+import { FaStar,FaStarHalfAlt,FaRegStar  } from 'react-icons/fa'
+
+import React from 'rea ct'
+
+const Rating = ({value,text}) => {
+  return (
+      <div className='rating'>
+    <span>
+      
+    </span>
+
+
+    </div>
+  )
+}
+
+export default Rating
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import{createBrowserRouter,
+   createRoutesFromElements,
+    Route,
+  RouterProvider,
+  } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomeScreen from './screen/HomeScreen';
+
+const route=createBrowserRouter(
+  createRoutesFromElements(
+        <Route path='/' element={<App/>}>
+         <Route index={true} path='/' element={<HomeScreen/>}></Route>
+        
+        </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={route}></RouterProvider>
   </React.StrictMode>
 );
 
